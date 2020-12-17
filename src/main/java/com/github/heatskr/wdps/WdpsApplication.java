7284457f8c654311df7f42bd315449f56afdb467
Giovanni Oliveira <giovanni.oliveira@gmail.com>
2020-12-17 14:44:23 -0300

    Spring Boot initializer

---
+++ src/main/java/com/github/heatskr/wdps/WdpsApplication.java
@@ -0,0 +1,13 @@
+package com.github.heatskr.wdps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WdpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/heatskr/wdps/WdpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.heatskr.wdps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WdpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
